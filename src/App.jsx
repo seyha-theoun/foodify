6441c9e7b13d14068bcb26583cb679e7b6e10a30
TheounSeyha <149511852+TheounSeyha@users.noreply.github.com>
2025-07-23 09:38:01 +0700

    add pageNotFound

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
           {/* Use Checkout here */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Sigup />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </CartProvider>
       {/* <Footer /> */}
